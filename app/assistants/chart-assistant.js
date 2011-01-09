58d9a749a0c76f128ced7ec5476200d7ae4233f7
micah <micah@44a92a27-b0d5-42b7-ae6a-fcca07464902>
2011-01-09 05:44:40 +0000

    Woo, basic charts are rendering in an actual app

--- app/assistants/chart-assistant.js
+++ app/assistants/chart-assistant.js
@@ -13,124 +13,73 @@
 	OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 	ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-var EngineAssistant = Class.create({
-
-/*
-	TODO:
-	- Keep track of the turtle's current position, and position the level
-	  relative to him.
-	- Only animate the turtle while he is moving
-	- Add additional properties to the tiles.  For example, can the turtle move
-	  through a tile, or is it solid.
-	- Implement controls.
-*/
+var ChartAssistant = Class.create({
 
 setup: function()
 {
-	this.controller.enableFullScreenMode(true);
-
 	/* I'd like consistent events, regardless of the orientation */
 	this.controller.useLandscapePageUpDown(false);
 
-
-	this.controller.listen(document, 'keyup',
-		this.keyUp.bindAsEventListener(this), true);
-	this.controller.listen(document, 'keydown',
-		this.keyDown.bindAsEventListener(this), true);
-	this.controller.listen(document, Mojo.Event.stageDeactivate,
-		this.background.bindAsEventListener(this), true);
-	this.controller.listen(document, Mojo.Event.stageActivate,
-		this.foreground.bindAsEventListener(this), true);
 	this.controller.listen(document, 'resize',
 		this.screenSizeChanged.bind(this));
+
+	// TODO Load the user's saved data and preferences (height and target
+	//		weight) from preferences
+	this.data			= [];
+	this.min			= 0;
+	this.max			= 0;
+
+	/* The user's height in inches */
+	this.height			= 71;
+
+	/* The user's target weight */
+	this.target			= 183;
+
+	this.scrollOffset	= 0;
+	this.topMargin		= 24;
+
+	/* The number of days to display.  Adjusting this will scale the chart. */
+	this.daycount		= 37;
+
+
+	// TODO Load real data instead of this fake data
+	/* Assign some random data */
+	var d = new Date(2011, 4, 27, 0, 0, 0, 0);
+	for (var i = 0; i < 32; i++) {
+		d.setHours(d.getHours() + 24 + Math.floor(Math.random() * 32));
+		this.data[this.data.length] = {
+			'weight':	(300 - (i / 4)) + (Math.random() * 3),
+			'date':		new Date(d.getTime())
+		};
+	}
+
+	/* Determine the smallest and largest values in our dataset */
+	this.min = this.target;
+	this.max = 0;
+
+	for (var i = 0; i < this.data.length; i++) {
+		this.min = Math.min(this.min, this.data[i].weight);
+		this.max = Math.max(this.max, this.data[i].weight);
+	}
+
+	this.max = Math.max(this.max, this.min + 50);
+	this.min -= 20;
+	this.max += 20;
+
+
+
+
+	/* This will be filled out during render() */
+	this.ctx			= null;
 },
 
 ready: function()
 {
-	/*
-		Set a starting position, the level will be positioned relative to the
-		player such that the player appears at the right starting position.
-	*/
-	this.player = [ 5, 5 ];
-
-	/* Load the level */
-	var l		= firstlevel;
-	var level	= this.controller.get('level');
-	var x		= 0;
-	var y		= 0;
-
-	l.layout.each(function(line)
-	{
-		x = 0;
-		for (var i = 0; i < line.length; i++, x++) {
-			var type	= line.charAt(i);
-			var div		= document.createElement('div');
-
-			if (l[type]) {
-				/* Let the level setup the div */
-				l[type](div);
-			}
-
-			div.style.position	= 'absolute';
-			div.style.left		= (x * 64) + 'px';
-			div.style.top		= (y * 64) + 'px';
-
-			level.appendChild(div);
-		}
-		y++;
-	});
-
-	/* Start animations */
-	this.body	= this.controller.get('body');
-	this.frame	= 0;
-
-	this.activate();
 	this.screenSizeChanged();
-},
-
-/* Called on a timer interval to render the new frame */
-render: function()
-{
-	Element.removeClassName(this.body, 'frame' + this.frame++ % 20);
-	Element.addClassName(	this.body, 'frame' + this.frame   % 20);
-},
-
-activate: function()
-{
-	this.foreground();
-},
-
-deactivate: function()
-{
-},
-
-foreground: function()
-{
-	if (!this.interval) {
-		this.interval = window.setInterval(this.render.bind(this), 50);
-	}
-},
-
-background: function()
-{
-	var		i;
-
-	if ((i = this.interval)) {
-		this.interval = null;
-		window.clearInterval(i);
-	}
 },
 
 cleanup: function()
 {
-	this.controller.stopListening(document, 'keyup',
-		this.keyUp.bindAsEventListener(this), true);
-	this.controller.stopListening(document, 'keydown',
-		this.keyDown.bindAsEventListener(this), true);
-	this.controller.stopListening(document, Mojo.Event.stageDeactivate,
-		this.background.bindAsEventListener(this), true);
-	this.controller.stopListening(document, Mojo.Event.stageActivate,
-		this.foreground.bindAsEventListener(this), true);
 	this.controller.stopListening(document, 'resize',
 		this.screenSizeChanged.bind(this));
 },
@@ -145,19 +94,7 @@ screenSizeChanged: function()
 		return;
 	}
 
-	var h		= parseInt(this.controller.window.innerHeight);
-	var w		= parseInt(this.controller.window.innerWidth);
-	var x		= Math.floor(w / 2);
-	var y		= Math.floor(h / 2);
-	var level	= this.controller.get('level');
-	var player	= this.controller.get('player');
-
-	player.style.left	= x + 'px';
-	player.style.top	= y + 'px';
-
-	/* Position the level based on the player's position */
-	level.style.left	= ((x - 32) - (this.player[0] * 64)) + 'px';
-	level.style.top		= ((y - 32) - (this.player[1] * 64)) + 'px';
+	this.render();
 },
 
 handleCommand: function(event)
@@ -191,19 +128,26 @@ handleCommand: function(event)
 		}
 	}
 
+
+	// TODO: Setup the menu.  Right now none of these events will be called
+	// because the default menu is in place
 	switch (cmd) {
 		case 'undo':
 			break;
 
 		case Mojo.Menu.prefsCmd:
+			// TODO: Create a prefs page
 			this.controller.stageController.pushScene('prefs');
 			break;
 
 		case 'about':
+			// TODO: Create an about page.  It has to give credit for the icon
+			// (see http://www.chris-wallace.com/2009/10/18/monday-freebie-vector-scale-icon/ )
 			this.controller.stageController.pushScene('about');
 			break;
 
 		case Mojo.Menu.helpCmd:
+			// TODO: Create a help page
 			this.controller.stageController.pushScene('help');
 			break;
 
@@ -217,12 +161,254 @@ handleCommand: function(event)
 	}
 },
 
-keyUp: function(event)
+render: function()
 {
+	var h			= parseInt(this.controller.window.innerHeight);
+	var w			= parseInt(this.controller.window.innerWidth);
+	var cv			= this.controller.get('weightchart');
+
+	cv.style.width	= w + 'px';
+	cv.style.height	= h + 'px';
+
+	cv.width		= w;
+	cv.height		= h;
+
+	this.ctx		= cv.getContext('2d');
+
+	/*
+		Adjust the canvas so that the origin is in the bottom left which matches
+		our grid.  The y axis is negative.
+	*/
+	this.ctx.save();
+	this.ctx.translate(0, h);
+
+
+
+	/* Draw grid lines */
+	this.ctx.save();
+	this.ctx.shadowStyle = 'rgba(0, 0, 0, 0)';
+
+	for (var y = (this.min - (this.min % 10)); y <= this.max; y += 10) {
+		// TODO WTF... The lines are like 5 pixels wide and blurry as shit...
+		//		grr... how the fuck do I fix this?
+		this.drawHorizLine(this.getY(y),     'rgba(46, 49, 52, 1)');
+		this.drawHorizLine(this.getY(y) + 1, 'rgba(81, 86, 91, 1)');
+	}
+	this.ctx.restore();
+
+
+
+	/* Setup some styles before drawing the data points and line */
+	this.ctx.save();
+
+	this.ctx.lineWidth		= 2;
+	this.ctx.lineCap		= 'round';
+	this.ctx.lineJoin		= 'round';
+	this.ctx.shadowOffsetX	= 2;
+	this.ctx.shadowOffsetY	= 2;
+	this.ctx.shadowColor	= 'rgba(0, 0, 0, 1)';
+
+
+	/* Draw the "Target" line */
+	this.drawHorizLine(this.getY(this.target), 'rgba(199, 121,  39, 1)');
+	this.showHint("TARGET (" + this.target + ")",
+		48, this.getY(this.target) - 2,
+		'rgba(255, 255, 255, 1)',
+		'rgba(199, 121,  39, 1)');
+
+	/*
+		Draw the "Weight" line.  This is the main line based on all the values
+		the user has previously entered.
+	*/
+	this.ctx.save();
+
+	this.ctx.strokeStyle		= 'rgba( 79, 121, 159, 1)';
+	this.ctx.fillStyle		= 'rgba( 79, 121, 159, 1)';
+
+	this.ctx.beginPath();
+	this.ctx.moveTo(0, this.getY(this.data[0].weight));
+	for (var i = 1; i < this.data.length; i++) {
+		this.ctx.lineTo(this.getX(this.data[i].date), this.getY(this.data[i].weight));
+	}
+	this.ctx.stroke();
+
+	/*
+		Draw a projected line...
+
+		TODO: Calculate a rate based on the last few days of data
+		and draw a projected line to the end of the graph based on
+		that rate.
+	*/
+	var tardate = new Date(this.data[this.data.length - 1].date.getTime());
+
+	/* Add 30 days... */
+	tardate.setDate(tardate.getDate() + 30);
+	this.ctx.dashedLineTo(
+		this.getX(this.data[this.data.length - 1].date),
+		this.getY(this.data[i - 1].weight),
+
+		this.getX(tardate), this.getY(this.data[i - 1].weight - 20),
+		[2, 7]);
+
+	this.ctx.stroke();
+
+	/* Draw a "dot" on each point */
+	for (var i = 0; i < this.data.length; i++) {
+		this.ctx.beginPath();
+		this.ctx.arc(	this.getX(this.data[i].date),
+						this.getY(this.data[i].weight),
+						4, 0, 360, true);
+		this.ctx.fill();
+	}
+
+	this.ctx.restore();
+
+	/* The lines are all complete now */
+	this.ctx.restore();
+
+
+	/* Restore the original origin */
+	this.ctx.restore();
+
+	/* this.ctx is only valid while rendering */
+	this.ctx		= null;
 },
 
-keyDown: function(event)
+
+/*
+	****************************************************************************
+	Helper functions used during rendering
+	****************************************************************************
+*/
+
+/*
+	Return a string version of a number with a single digit after the decimal
+	point.  No more, no less.
+*/
+NumToStr: function(num)
 {
+	var n	= Math.floor(num * 10) / 10;
+	var str	= "" + n;
+
+	if (Math.floor(n) == n) {
+		str += ".0";
+	}
+	return(str);
+},
+
+KGtoBMI: function(kgs, meters)
+{
+	return(kgs / (meters * meters));
+},
+
+LBtoBMI: function(lbs, inches)
+{
+	return((lbs * 703) / (inches * inches));
+},
+
+
+/* Return the Y offset on the graph based on a weight */
+getY: function(weight)
+{
+	var h		= parseInt(this.controller.window.innerHeight);
+	var r		= (h - this.topMargin) / (this.max - this.min);
+
+	return(-((weight - this.min) * r));
+},
+
+/* Return the X offset on the graph based on a specific date */
+getX: function(date)
+{
+	var w			= parseInt(this.controller.window.innerWidth);
+	var daywidth	= (w / this.daycount);
+	var start		= this.data[0].date.getTime();
+	var end			= date.getTime();
+
+	var days		= (end - start) / (86400000);
+
+	/* Pad the first point by 32 for the weight labels */
+	return((32 + (days * daywidth)) - this.scrollOffset);
+},
+
+/*
+	Do the inverse of the getX function above.  Take an offset and return a
+	date object set properly based on the location on the graph.
+
+	The timestamp of the first data point is fixed at 32 pixels.
+*/
+getDate: function(x)
+{
+	var w			= parseInt(this.controller.window.innerWidth);
+	var daywidth	= (w / this.daycount);
+	var ms			= ((x - 32) / daywidth) * (86400000);
+
+	return(new Date(this.data[0].date.getTime() + ms));
+},
+
+/*
+	Draw a simple horizontal line all the way across the chart at the specified
+	height.
+*/
+drawHorizLine: function(y, style)
+{
+	if (!this.ctx) return;
+
+	var w			= parseInt(this.controller.window.innerWidth);
+
+	this.ctx.save();
+	if (style) {
+		this.ctx.strokeStyle = style;
+	}
+	// this.ctx.lineWidth = 0.0001;
+
+	this.ctx.beginPath();
+// TODO: I don't want to do this floor here...
+	this.ctx.moveTo(0, Math.floor(y));
+	this.ctx.lineTo(w, Math.floor(y));
+
+	this.ctx.stroke();
+	this.ctx.restore();
+},
+
+showHint: function(text, x, y, fgstyle, bgstyle)
+{
+	if (!this.ctx) return;
+
+	this.ctx.save();
+
+	this.ctx.fillStyle		= bgstyle;
+	this.ctx.textAlign		= 'left';
+	this.ctx.shadowOffsetX	= 2;
+	this.ctx.shadowOffsetY	= 2;
+	this.ctx.shadowColor	= 'rgba(0, 0, 0, 1)';
+
+	var width				= this.ctx.measureText(text).width;
+
+	this.ctx.beginPath();
+	this.ctx.moveTo(x, y);
+	this.ctx.lineTo(x + 2, y - 5);
+
+	this.ctx.lineTo(x - 5, y - 5);
+	this.ctx.lineTo(x - 5, y - 25);
+	this.ctx.lineTo(x + width + 5, y - 25);
+	this.ctx.lineTo(x + width + 5, y - 5);
+
+	this.ctx.lineTo(x + 15, y - 5);
+	this.ctx.closePath();
+
+	this.ctx.fill();
+
+	/* Don't draw a shadow */
+	this.ctx.shadowOffsetX	= 0;
+	this.ctx.shadowOffsetY	= 0;
+	this.ctx.shadowBlur		= 0;
+
+	this.ctx.fillStyle		= fgstyle;
+	this.ctx.font			= 'bold 10px sans-serif';
+
+	this.ctx.fillText(text, x - 1, y - 12);
+
+	this.ctx.restore();
 }
 
 });
